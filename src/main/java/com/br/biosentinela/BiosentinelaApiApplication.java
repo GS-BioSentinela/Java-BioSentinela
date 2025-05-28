f665f25de3fcac58b7ce293a8d4a4541f1f73cd5
GomesMancera <manceragomesbr@gmail.com>
2025-05-28 15:20:14 -0300

    🎉 Projeto inicial gerado com Spring Initializr

---
+++ src/main/java/com/br/biosentinela/BiosentinelaApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.biosentinela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiosentinelaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiosentinelaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/biosentinela/BiosentinelaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.biosentinela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiosentinelaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
