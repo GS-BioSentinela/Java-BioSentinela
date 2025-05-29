3e671ce78bf4d680d39e590a5ac16b63860b12a4
GomesMancera <manceragomesbr@gmail.com>
2025-05-28 22:33:25 -0300

    feat(auth): adiciona entidade Usuario e repository para autenticação JWT

---
+++ src/main/java/com/br/biosentinela/model/Usuario.java
@@ -0,0 +1,23 @@
+package com.br.biosentinela.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
